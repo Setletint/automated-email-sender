3bd955b26924706a40b844a0194226285379d936
Setletint <polikq772@gmail.com>
2023-11-27 21:21:47 +0200

    Made class Database
    Created class for mongoDB.
    Made functions to get all collections, insert new user & delete user

---
+++ database/config.py
@@ -0,0 +1,29 @@
+import pymongo
+
+class Database:
+    def __init__(self) -> None:
+        self.conn = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.db_email = self.conn["emails"]
+        self.users = self.db_email["users"]
+        
+    def getAll(self):
+        print(self.conn.list_database_names())
+        
+    def insertNewUser(self, name, mail):
+        exist = self.users.count_documents({'email':mail})
+        if(exist > 0):
+            return 'Already exist'
+        else:
+            user = {'user': name, 'email':mail}
+            self.users.insert_one(user)
+
+    def deleteUser(self, mail):
+        userD = {'email':mail}
+        exist = self.users.find({},userD)
+        if(exist):
+            self.users.delete_one(userD)
+            
+
+obj = Database()
+obj.insertNewUser('SuzuCudzi','test@test.com')
+obj.getAll()
