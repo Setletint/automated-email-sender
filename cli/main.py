1e2055b44d61e83ade5f9ec48f14ecca2f4a9adf
Setletint <polikq772@gmail.com>
2023-11-29 00:19:18 +0200

    Added cli function to change mail in user config
    To run it use python /cli/main.py changeConfig --mail YourMailHere

--- cli/main.py
+++ cli/main.py
@@ -1,1 +1,28 @@
-from database.config import Database as db
+#from database.config import Database as db
+import click
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option('--mail')
+@click.option('--password')
+def changeConfig(mail,password):
+        
+        if(mail):
+            try:
+                with open('user-config.txt','r') as f:
+                    userConfig = f.read().splitlines()
+                    oldPass = userConfig[1]
+                with open('user-config.txt', 'w') as f:
+                    f.write(f'{mail}\n'+oldPass)
+            except:
+                with open('user-config.txt','w') as f:
+                    f.write(f'{mail}\n')
+                
+
+cli.add_command(changeConfig)
+
+if __name__ == '__main__':
+    cli()
